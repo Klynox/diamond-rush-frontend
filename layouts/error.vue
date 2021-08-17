d07ee3f08a7141b9266f111f196f38fd9193a960
Confidence Yobo <confiyobo@gmail.com>
2021-08-17 04:26:27 -0500

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <PageErrorCard :homeBtn="true">
+      <h3 v-if="error.statusCode === 404">Page not found</h3>
+      <h3 v-else>An error occurred</h3>
+      <p>it seems you have visited the wrong URL</p>
+    </PageErrorCard>
+  </div>
+</template>
+
+<script>
+import PageErrorCard from "@/components/cards/page-error-card";
+export default {
+  props: ["error"],
+  layout: "default",
+  components: {
+    PageErrorCard,
+  },
+};
+</script>
